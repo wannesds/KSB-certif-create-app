7f703fe04189575f9b4d1f73505a5197e78ba186
wannesds <wannesdieltiens@gmail.com>
2021-05-13 20:04:38 +0200

    initialize, added login, logout, and todo button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import { LoginButton, LogoutButton, Text, useSession, CombinedDataProvider } from "@inrupt/solid-ui-react";
+import AddTodo from "./components/AddTodo/AddTodo";
+
+const authOptions = {
+    clientName: "Solid Todo App",
+  };
 
 function App() {
+  const { session } = useSession();
+  const [oidcIssuer, setOidcIssuer] = useState("");
+
+  const handleChange = (event) => {
+    setOidcIssuer(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app-container">
+      {session.info.isLoggedIn ? ( //logged in?
+        <CombinedDataProvider
+          datasetUrl={session.info.webId}
+          thingUrl={session.info.webId}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="message logged-in">
+            <span>You are logged in as: </span>
+            <Text properties={[
+                "http://www.w3.org/2006/vcard/ns#fn",
+                "http://xmlns.com/foaf/0.1/name",
+              ]} />
+              <LogoutButton />
+          </div>
+          <section>
+            <AddTodo />
+          </section>
+        </CombinedDataProvider>
+      ) : ( //if not logged in then
+        <div className="message">
+          <span>You are not logged in. </span>
+          <span>
+            Log in with:
+            <input
+              className="oidc-issuer-input "
+              type="text"
+              name="oidcIssuer"
+              list="providers"
+              value={oidcIssuer}
+              onChange={handleChange}
+            />
+           <datalist id="providers">
+             <option value="https://broker.pod.inrupt.com/" />
+             <option value="https://inrupt.net/" />
+           </datalist>
+          </span>
+          <LoginButton
+            oidcIssuer={oidcIssuer}
+            redirectUrl={window.location.href}
+            authOptions={authOptions}
+          />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/AddTodo/AddTodo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddTodo() {
+  return <button className="add-button">Add Todo</button>;
+}
+
+export default AddTodo;
