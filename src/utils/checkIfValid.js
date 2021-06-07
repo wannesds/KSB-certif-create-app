6a86c1dc4ff09909e6cac08939ec7c95e7b6690d
Wannes Dieltiens <50752071+wannesds@users.noreply.github.com>
2021-06-07 14:03:35 +0200

    sync

--- src/utils/checkIfValid.js
+++ src/utils/checkIfValid.js
@@ -4,16 +4,15 @@ import CreateHash  from '../utils/createHash';
 function CheckIfValid(data, txnList){
     //hash === hash stored in Txn ? -> return 'true'
     const array = txnList.result;
+    console.log('array: ', array)
     const hash = CreateHash(data.webID, data.certifID, (res => res))
 
-    const validTxn = array.find((txn) => txn.input === hash)
+    const validTxn = array.find((txn) => txn.input === '0x' + hash)
+    const state = validTxn ? true : false
 
     console.log('validTxn', validTxn)
 
-    var valid = "";
-    data? valid = true : valid = false;
-
-    return data;
+    return state;
 }
 
 export default CheckIfValid;
